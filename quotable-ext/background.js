9480d00d27a468f60a86cae472137493ca51a1a1
Emanuel Saringan <dev@lambdageek.com>
2014-07-26 15:15:40 +0800

    Updated comment

--- quotable-ext/background.js
+++ quotable-ext/background.js
@@ -1,42 +1,43 @@
 chrome.contextMenus.create({
-  title: 'Send to Quotable',
-  contexts:['selection'],
-  onclick: function(info) {
-    // Get highlighted text
-    chrome.tabs.executeScript( {
-      code: 'window.getSelection().toString();'
-    }, function(selection) {
-      if (selection.length <= 0) {
-        return;
-      }
+    title: 'Send to Quotable',
+    contexts:['selection'],
+    onclick: function(info) {
+        // Get highlighted text
+        chrome.tabs.executeScript( {
+            code: 'window.getSelection().toString();'
+        }, function(selection) {
+            if (selection.length <= 0) {
+                return;
+            }
 
-      chrome.tabs.getSelected(null,
-        function(tab) {
-          // Send to Quotable
-          var http = new XMLHttpRequest();
-          var url = 'http://localhost:3000/quote';
+            // Get page info
+            chrome.tabs.getSelected(null,
+                function(tab) {
+                    // Send to Quotable
+                    var http = new XMLHttpRequest();
+                    var url = 'http://localhost:3000/quote';
 
-          // Construct query string
-          var params = 'text=' + encodeURIComponent(selection[0]);
-          params += '&title=' + encodeURIComponent(tab.url);
-          params += '&url=' + encodeURIComponent(tab.title);
-          http.open('POST', url, true);
+                    // Construct query string
+                    var params = 'text=' + encodeURIComponent(selection[0]);
+                    params += '&title=' + encodeURIComponent(tab.url);
+                    params += '&url=' + encodeURIComponent(tab.title);
+                    http.open('POST', url, true);
 
-          // Construct header
-          http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-          http.setRequestHeader('Content-length', params.length);
-          http.setRequestHeader('Connection', 'close');
+                    // Construct header
+                    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+                    http.setRequestHeader('Content-length', params.length);
+                    http.setRequestHeader('Connection', 'close');
 
-          // Handle error
-          http.onreadystatechange = function() {
-              if (http.readyState == 4 && http.status == 200) {
-                  alert(http.responseText);
-              }
-          };
+                    // Handle error
+                    http.onreadystatechange = function() {
+                            if (http.readyState == 4 && http.status == 200) {
+                                    alert(http.responseText);
+                            }
+                    };
 
-          http.send(params);
-        }
-      );
-    });
-  }
+                    http.send(params);
+                }
+            );
+        });
+    }
 });
