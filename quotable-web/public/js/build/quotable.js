92804bc6e96d4ab8406cfedfc0f9b5de5dfdb6c0
Emanuel Saringan <dev@lambdageek.com>
2014-07-26 23:32:59 +0800

    Changed color

--- quotable-web/public/js/build/quotable.js
+++ quotable-web/public/js/build/quotable.js
@@ -45,8 +45,8 @@ var QuoteBox = React.createClass({displayName: 'QuoteBox',
     render: function() {
         return (
             React.DOM.div({className: "quoteBox"}, 
-                QuoteTitle({title: this.props.quotable.title, url: this.props.quotable.url}), 
                 QuoteText({text: this.props.quotable.text}), 
+                QuoteTitle({title: this.props.quotable.title, url: this.props.quotable.url}), 
                 QuoteTime({time: this.props.quotable.createdAt}), 
                 React.DOM.hr(null)
             )
