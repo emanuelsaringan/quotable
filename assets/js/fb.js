375160bb406b939b0f22cd246078e5a2ae42f29a
Emanuel Saringan <dev@lambdageek.com>
2014-08-03 09:47:27 +0800

    Added FB sharing dialog

---
+++ assets/js/fb.js
@@ -0,0 +1,33 @@
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '721191361280674',
+    xfbml      : true,
+    version    : 'v2.0'
+  });
+
+  $('.fb-share').click(
+    function() {
+      FB.ui(
+        {
+          method: 'share',
+          href: 'https://developers.facebook.com/docs/',
+        },
+        function(response) {
+          if (response && !response.error_code) {
+            alert('Posting completed.');
+          } else {
+            alert('Error while posting.');
+          }
+        }
+      );
+    }
+  );
+};
+
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "http://connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
